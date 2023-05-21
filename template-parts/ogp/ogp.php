c2d428efdaa91baf6aea9f58cc518d0fca3b5f8b
boro2jp <boro2.jp@gmail.com>
2023-05-21 15:47:14 +0900

    レイアウト修正

---
+++ template-parts/ogp/ogp.php
@@ -0,0 +1,8 @@
+
+<head prefix="og: https://ogp.me/ns#">
+<meta property="og:url" content="<?php echo get_the_permalink();?>" />
+<meta property="og:type" content="article" />
+<meta property="og:title" content="<?php echo get_the_archive_title() ?>" />
+<meta property="og:description" content="<?php echo get_bloginfo( 'description' ) ?>" />
+<meta property="og:site_name" content="<?php echo get_bloginfo( 'name' ) ?>" />
+<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/assets/images/ogp/logo.jpg" />
