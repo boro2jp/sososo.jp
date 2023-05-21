9cd4d4ea0be7c4af1b4192cfb479dd21c7cdec8a
boro2jp <boro2.jp@gmail.com>
2023-05-21 13:13:49 +0900

    ファビコン修正

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
-    <link rel=”icon” href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/favicon.ico">
 	<?php get_template_part('template-parts/ogp');?>
 </head>
 <body <?php body_class(); ?>>
